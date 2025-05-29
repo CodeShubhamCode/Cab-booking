4d39878879c24a9a00cd3d6b22b683eb7dd64e24
Shubham Jha <155728598+CodeShubhamCode@users.noreply.github.com>
2025-05-29 13:19:17 +0530

    Update CaptainProtectWrapper.jsx

--- Frontend/src/pages/CaptainProtectWrapper.jsx
+++ Frontend/src/pages/CaptainProtectWrapper.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CaptainDataContext } from '../context/captaincontext'
+import { CaptainDataContext } from '../context/CaptainContext'
 import { useNavigate } from 'react-router-dom'
 import { useContext,useEffect } from 'react'
 import axios from 'axios'
