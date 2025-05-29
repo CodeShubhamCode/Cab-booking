7bbf7090eac88956b634e76ca4a36a8acac18ab9
Shubham Jha <155728598+CodeShubhamCode@users.noreply.github.com>
2025-05-29 13:26:30 +0530

    Update CaptainLogin.jsx

--- Frontend/src/pages/CaptainLogin.jsx
+++ Frontend/src/pages/CaptainLogin.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { CaptainDataContext } from "../context/captaincontext";
+import { CaptainDataContext } from "../context/CaptainContext";
 import { SocketContext } from "../context/SocketContext"; // Added import
 
 const CaptainLogin = () => {
