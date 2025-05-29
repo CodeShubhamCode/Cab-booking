4eab0a074198a11a48718998f6ede666129b2f73
Shubham Jha <155728598+CodeShubhamCode@users.noreply.github.com>
2025-05-29 13:28:21 +0530

    Update CaptainDetails.jsx

--- Frontend/src/components/CaptainDetails.jsx
+++ Frontend/src/components/CaptainDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { CaptainDataContext } from "../context/captaincontext.jsx";
+import { CaptainDataContext } from "../context/CaptainContext";
 
 const CaptainDetails = () => {
   const { captain } = useContext(CaptainDataContext);
